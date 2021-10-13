51356d06149305097461f71f585c9c37e124d902
Hanqing1234 <lhq3361951@gmail.com>
2021-10-13 12:53:52 -0400

    test heroku

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Secret = exports.RemoteURI = exports.HostName = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://localhost/store";
 exports.HostName = "remotehost";
-exports.RemoteURI = process.env.RemoteURI;
+exports.RemoteURI = "mongodb+srv://hanqing:lhq3361951@hanqing.xas3b.mongodb.net/store?retryWrites=true&w=majority";
 exports.Secret = "someSecret";
 //# sourceMappingURL=db.js.map
--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,4 +1,4 @@
 export const LocalURI = "mongodb://localhost/store";
 export const HostName = "remotehost";
-export const RemoteURI = process.env.RemoteURI;
+export const RemoteURI = "mongodb+srv://hanqing:lhq3361951@hanqing.xas3b.mongodb.net/store?retryWrites=true&w=majority";
 export const Secret = "someSecret";
